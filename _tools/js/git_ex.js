28e417ef0d19f833a000e129327eb25d8730ff3f
Gediminas <gediminas.luzys@gmail.com>
2021-01-10 10:54:22 +0200

    Simplify logging

--- _tools/js/git_ex.js
+++ _tools/js/git_ex.js
@@ -3,44 +3,45 @@ const _fs    = require('fs');
 const { exec } = require('./sys.js');
 
 exports.getclean_repo = (repo, dir, branch) => {
-  console.log('  => git_ex -> getclean_repo');
+  console.log('* git_ex -> getclean_repo');
 
   const git_check_path = _path.resolve(dir + '/.git/refs/heads');
 
-  const opts = {
-    stdio: 'inherit',
-  };
+  // const opts = {
+  //   stdio: 'inherit',
+  // };
+  const opts = {};
 
   if (_fs.existsSync(git_check_path)) {
-    console.log('> Repo exists. Updating...');
+    console.log('Repo exists. Updating...');
     opts.cwd = dir;
     exec('git', ['fetch', '--all'], opts);
     exec('git', ['reset', '--hard', `origin/${branch}`], opts);
     exec('git', ['clean', '-fdx'], opts);
-    exec('git', ['pull'], opts, 'Updating repo');
+    exec('git', ['pull'], opts);
   } else {
-    console.log('  // Repo does not exist. Cloning...');
+    console.log('Repo does not exist. Cloning...');
     // FIXME: git clone sends progress to stderr
-    // exec('git', ['clone', '--progress', '--recursive', '-b', branch, repo, dir], opts, 'Cloning git repository');
-    exec('git', ['clone', '--quiet', '--recursive', '-b', branch, repo, dir], opts, 'Cloning git repository');
+    // exec('git', ['clone', '--progress', '--recursive', '-b', branch, repo, dir], opts);
+    exec('git', ['clone', '--quiet', '--recursive', '-b', branch, repo, dir], opts);
   }
   return true;
 };
 
 exports.getclean = (repo, dir, branch) => {
   // console.log(`@sub GetClean(${repo}, ${dir}, ${branch})`);
-  console.log('  => git_ex -> getclean');
+  console.log('* git_ex -> getclean');
 
   const resolved_dir    = _path.resolve(dir);
   const resolved_branch = (branch === undefined) ? 'master' : branch;
 
-  console.log(`  :: repo   = ${repo}`);
-  console.log(`  :: dir    = ${resolved_dir}`);
-  console.log(`  :: branch = ${resolved_branch}`);
+  console.log(`repo   = ${repo}`);
+  console.log(`dir    = ${resolved_dir}`);
+  console.log(`branch = ${resolved_branch}`);
 
   const ret = exports.getclean_repo(repo, resolved_dir, resolved_branch);
 
   // console.log(`  :: result : ${ret}`);
   // console.log('@end');
   return ret;
-}
+};
--- _tools/js/sys.js
+++ _tools/js/sys.js
@@ -2,27 +2,22 @@ const child_process = require('child_process');
 
 const asleep = (ms) => new Promise((resolve) => setTimeout(resolve, ms));
 
-const exec = (file, args, options, comment) => {
-    //const opts_line = options ? options.join(' ') : '';
-    //const args_line = args ? args.join(' ') : '';
-    if (comment) {
-        console.log('  //', comment);
-    }
-    if (options) {
-        console.log('  // options =', JSON.stringify(options));
-    }
-    console.log('  >> ', file, args.join(' '));
+const exec = (file, args, options) => {
+  // if (options) {
+  //   console.log('options =', JSON.stringify(options));
+  // }
 
-    try {
-        child_process.execFileSync(file, args, options);
-    } catch (e) {
-        console.error(e.toString());
-        return false;
-    }
+  try {
+    console.log('>>', file, args.join(' '));
+    child_process.execFileSync(file, args, options);
     return true;
-}
+  } catch (e) {
+    console.error(e.toString());
+    return false;
+  }
+};
 
 module.exports = {
-    asleep,
-    exec,
+  asleep,
+  exec,
 };
