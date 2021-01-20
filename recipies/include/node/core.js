dcaefffe33c119a4e9f168b07125efa76f2c8cfc
Gediminas <gediminas.luzys@gmail.com>
2021-01-20 16:47:34 +0200

    pass date folder by param

--- recipies/include/node/core.js
+++ recipies/include/node/core.js
@@ -2,7 +2,7 @@ const path = require('path');
 const sys = require('./sys.js');
 
 const get_temp = (script) => {
-  const temp = path.resolve('_working', script, '2021-01-14_23-20-00');
+  const temp = path.resolve('../_working', script, '2021-01-14_23-20-00');
   sys.ensureDir(temp);
   return temp;
 };
