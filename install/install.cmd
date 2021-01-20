409a2655cfcf059d4736801c6bfcad0a62723b21
Gediminas <gediminas.luzys@gmail.com>
2021-01-20 15:21:13 +0200

    install fix

--- install/install.cmd
+++ install/install.cmd
@@ -1,6 +1,11 @@
 @echo off
+
 assoc .=shebangfile
+
+pushd ..
 ftype shebangfile=%cd%\bin\shebang.cmd "%%1" %%*
+popd
+
 setx /m PATHEXT %PATHEXT%;.
 setx PATHEXT %PATHEXT%;.
 set PATHEXT=%PATHEXT%;.
