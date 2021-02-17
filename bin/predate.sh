33bc53e5f3c2a560bda1ce3cc74dfd50ec1f5ff9
Gediminas <gediminas.luzys@gmail.com>
2021-02-17 16:27:17 +0200

    Cleanup

--- bin/predate.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-echo "PREDATE"
-
-while IFS= read -r line; do
-  #echo "$(date +%F %H:%M:%S) $line"
-  echo "$(date '+%Y-%m-%d %H:%M:%S') $line"
-  # printf "%s: %s\n" "$(date '+%Y-%m-%d %H:%M:%S')" "$line"
-done
