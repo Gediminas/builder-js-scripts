a1834f954f1e2d2ef51a9c12109bb55a2e2bcdb1
Gediminas <gediminas.luzys@gmail.com>
2021-02-25 22:59:55 +0200

    timeout function (broken)

--- recipies/library/git_get.sh
+++ recipies/library/git_get.sh
@@ -1,38 +1,42 @@
-#!/usr/bin/env bash
+#!/usr/bin/env sh
 
 # set -e  # exit when any command fails
 
-repo=$1
-dir=$2
-branch=$3
-shift  # remove $1 from $@
-shift  # remove $2 from $@
-shift  # remove $3 from $@
+git_get() {
+    repo=$1
+    dir=$2
+    branch=$3
+    shift  # remove $1 from $@
+    shift  # remove $2 from $@
+    shift  # remove $3 from $@
 
 
-echo "## GIT GET MAIN REPO"
+    echo "## GIT GET MAIN REPO / $repo / $branch"
 
-git_check_path="$dir/.git/refs/heads"
-if [ -e "$git_check_path" ]; then
-    echo ">> git checkout -- ."
-    git checkout -- .
+    git_check_path="$dir/.git/refs/heads"
+    if [ -e "$git_check_path" ]; then
+        echo ">> git checkout -- ."
+        git checkout -- .
 
-    echo ">> git fetch --all --verbose --progress"
-    git fetch --all --verbose --progress
+        echo ">> git fetch --all --verbose --progress"
+        git fetch --all --verbose --progress
 
-    echo ">> git reset --hard "origin/${branch}" --verbose --progress"
-    git reset --hard "origin/${branch}"
+        echo ">> git reset --hard "origin/${branch}" --verbose --progress"
+        git reset --hard "origin/${branch}"
 
-    echo ">> git clean -fdx"
-    git clean -fdx
+        echo ">> git clean -fdx"
+        git clean -fdx
 
-    echo ">> git pull --verbose --progress"
-    git pull --verbose --progress
-else
-    echo ">> git clone "$repo" "$dir" -b "$branch" $@"
-    git clone "$repo" "$dir" -b "$branch" $@
-fi
+        echo ">> git pull --verbose --progress"
+        git pull --verbose --progress
+    else
+        echo ">> git clone "$repo" "$dir" -b "$branch" $@"
+        git clone "$repo" "$dir" -b "$branch" $@
+    fi
 
 
-# echo "## GIT GET \"SUBMODULES\""
-# sh ./get_submodules.sh --clean --quiet "$@"
+    # echo "## GIT GET \"SUBMODULES\""
+    # sh ./get_submodules.sh --clean --quiet "$@"
+}
+
+export -f git_get
---
+++ recipies/library/std.sh
@@ -0,0 +1,67 @@
+#!/usr/bin/env bash
+## forking is evil
+TTL () {
+    time=$1; shift
+    $@ & local pid=$! start=0
+    while kill -0 $pid; do
+        read -t 1
+        start=$((start+1))
+        if [ $start -ge $time ]; then
+            echo -e "\nERROR: Timeout $time s"
+            kill $pid && break
+        fi
+    done
+}
+
+TTLc () {
+    trap -- "" SIGTERM
+
+    local pid=$!
+    local t=$1
+    # local s=$((m * 60))
+
+    (
+        sleep $t
+        echo -e "\nERROR: Timeout $t s"
+        kill $pid
+    ) &
+    wait $pid
+}
+
+TTLa () {
+    m=$1; shift
+    s=$((m * 60))
+    echo ">> $@  [TTL=${m}m]"
+    timeout $s "$@"
+    ret=$?
+    case $ret in
+          0) ;;
+        124) echo "ERROR: Time Out ${m}m: $@" ;;
+          *) echo "[WARN] Exit code $ret" ;;
+    esac
+    return $ret
+}
+
+TTLb () {
+    local arr=( "$@" )
+    local timeout="${arr[0]}"
+    local cmd=( "${arr[@]:1}" )
+
+    echo "timeout=$timeout"
+    echo ">> ${cmd[@]}  [TTL=${timeout}s]"
+
+    (
+        eval "${cmd[@]}" &
+        pid=$!
+
+        echo "child pid: $pid"
+        trap -- "" SIGTERM
+        (
+            sleep "$timeout"
+            kill "$pid" 2> /dev/null
+            echo $?
+        ) &
+        wait "$pid"
+    )
+}
+
