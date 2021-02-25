a857680a16e81c236f321e920821ad3987e2003f
Gediminas <gediminas.luzys@gmail.com>
2021-02-25 14:20:28 +0200

    TTL, Exit on error, git_get

---
+++ recipies/library/git_get.sh
@@ -0,0 +1,38 @@
+#!/usr/bin/env bash
+
+# set -e  # exit when any command fails
+
+repo=$1
+dir=$2
+branch=$3
+shift  # remove $1 from $@
+shift  # remove $2 from $@
+shift  # remove $3 from $@
+
+
+echo "## GIT GET MAIN REPO"
+
+git_check_path="$dir/.git/refs/heads"
+if [ -e "$git_check_path" ]; then
+    echo ">> git checkout -- ."
+    git checkout -- .
+
+    echo ">> git fetch --all --verbose --progress"
+    git fetch --all --verbose --progress
+
+    echo ">> git reset --hard "origin/${branch}" --verbose --progress"
+    git reset --hard "origin/${branch}"
+
+    echo ">> git clean -fdx"
+    git clean -fdx
+
+    echo ">> git pull --verbose --progress"
+    git pull --verbose --progress
+else
+    echo ">> git clone "$repo" "$dir" -b "$branch" $@"
+    git clone "$repo" "$dir" -b "$branch" $@
+fi
+
+
+# echo "## GIT GET \"SUBMODULES\""
+# sh ./get_submodules.sh --clean --quiet "$@"
