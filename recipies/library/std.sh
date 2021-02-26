1e6e246af265c1cb7ed88e18889351ae1e571b34
Gediminas <gediminas.luzys@gmail.com>
2021-02-26 09:27:43 +0200

    sh: TTL simplified

--- recipies/library/std.sh
+++ recipies/library/std.sh
@@ -1,73 +1,78 @@
 #!/usr/bin/env bash
 ## forking is evil
 TTL () {
-    time=$1; shift
+    local -i time=$1; shift
+    # $@ & local pid=$! start=0
     $@ & local pid=$! start=0
-    while true; do
-        kill -0 $pid >/dev/null 2>&1
-        if [ "$?" == "1" ]; then
-            break
-        fi
+    while kill -0 $pid >/dev/null 2>&1; do
         read -t 1
         start=$((start+1))
         if [ $start -ge $time ]; then
             echo -e "\nERROR: Timeout $time s"
-            kill $pid && break
+
+            # kill $pid >/dev/null 2>&1
+
+            # kill $pid && break
+
+            # echo "kill gracefully"
+            # kill -s SIGTERM $pid && kill -0 $pid || exit 0
+            # sleep 1
+            # echo "kill brutaly"
+            # kill -s SIGKILL $pid
         fi
     done
 }
 
 # export -f TTL
 
-TTLa () {
-    m=$1; shift
-    s=$((m * 60))
-    echo ">> $@  [TTL=${m}m]"
-    timeout $s $@
-    ret=$?
-    case $ret in
-          0) ;;
-        124) echo "ERROR: Time Out ${m}m: $@" ;;
-          *) echo "[WARN] Exit code $ret" ;;
-    esac
-    return $ret
-}
+# TTLa () {
+#     m=$1; shift
+#     s=$((m * 60))
+#     echo ">> $@  [TTL=${m}m]"
+#     timeout $s $@
+#     ret=$?
+#     case $ret in
+#           0) ;;
+#         124) echo "ERROR: Time Out ${m}m: $@" ;;
+#           *) echo "[WARN] Exit code $ret" ;;
+#     esac
+#     return $ret
+# }
 
-TTLb () {
-    local arr=( "$@" )
-    local timeout="${arr[0]}"
-    local cmd=( "${arr[@]:1}" )
+# TTLb () {
+#     local arr=( "$@" )
+#     local timeout="${arr[0]}"
+#     local cmd=( "${arr[@]:1}" )
 
-    echo "timeout=$timeout"
-    echo ">> ${cmd[@]}  [TTL=${timeout}s]"
+#     echo "timeout=$timeout"
+#     echo ">> ${cmd[@]}  [TTL=${timeout}s]"
 
-    (
-        eval "${cmd[@]}" &
-        pid=$!
+#     (
+#         eval "${cmd[@]}" &
+#         pid=$!
 
-        echo "child pid: $pid"
-        trap -- "" SIGTERM
-        (
-            sleep "$timeout"
-            kill "$pid" 2> /dev/null
-            echo $?
-        ) &
-        wait "$pid"
-    )
-}
+#         echo "child pid: $pid"
+#         trap -- "" SIGTERM
+#         (
+#             sleep "$timeout"
+#             kill "$pid" 2> /dev/null
+#             echo $?
+#         ) &
+#         wait "$pid"
+#     )
+# }
 
-TTLc () {
-    trap -- "" SIGTERM
+# TTLc () {
+#     trap -- "" SIGTERM
 
-    local pid=$!
-    local t=$1
-    # local s=$((m * 60))
+#     local pid=$!
+#     local t=$1
+#     # local s=$((m * 60))
 
-    (
-        sleep $t
-        echo -e "\nERROR: Timeout $t s"
-        kill $pid
-    ) &
-    wait $pid
-}
-
+#     (
+#         sleep $t
+#         echo -e "\nERROR: Timeout $t s"
+#         kill $pid
+#     ) &
+#     wait $pid
+# }
