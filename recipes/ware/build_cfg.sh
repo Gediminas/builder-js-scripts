f708a0bcb8d0bdc73c36d8b7d95f7147db475aad
Gediminas <gediminas.luzys@gmail.com>
2021-03-07 14:07:05 +0200

    sh: Delete collect projects

--- recipes/ware/build_cfg.sh
+++ recipes/ware/build_cfg.sh
@@ -1,51 +1,5 @@
 #!/usr/bin/env bash
 
-__matching_project_data() {
-    local path_dsp="$1"
-    local configs="$2"
-    local platform="$3"
-    IFS=, configs=("${configs[@]}")
-    for config in $configs; do
-        if [[ ! -f "$path_dsp" ]]; then
-            echo "ERROR: '$path_dsp' does not exist" >&2
-            continue
-        fi
-        local needle="<ProjectConfiguration Include=\"$config|$platform\">"
-        local found=$(grep "$needle" < "$path_dsp")
-        if [[ -n $found ]]; then
-            echo "$path_dsp|$config|$platform"
-        fi
-    done
-}
-
-__project_list() {
-    local build_cfg="$1"
-    local configs="$2"
-    local platform="$3"
-    local build_cfg=$(realpath --no-symlinks $1)
-    local ROOT="${build_cfg%[/\\]*}"
-    local lines
-    readarray -t lines < "$build_cfg"
-    for line in ${lines[@]}; do
-        line=${line//[\ $'\r']}
-        if [[ -z "$line" ]]; then
-           continue # Skip empty lines
-        fi
-        if [[ $str =~ ^# ]]; then
-           continue # Skip commented lines
-        fi
-        if [[ $str =~ ^// ]]; then
-           continue # Skip commented lines
-        fi
-        path=$(realpath --no-symlinks "$ROOT/$line")
-        if [[ $path =~ ".cfg" ]]; then
-            __project_list "$path" "$configs" "$platform"
-        else
-            __matching_project_data "$path" "$configs" "$platform"
-        fi
-    done
-}
-
 build_cfg() {
     local build_cfg="$1"
     local configs="$2"
@@ -60,28 +14,20 @@ build_cfg() {
     build_list=$(realpath --no-symlinks $build_list)
     collect_builds=$(realpath --no-symlinks $collect_builds)
 
-    echo "~ build_list: $build_list"
-    echo "~ collect_builds: $collect_builds"
+        TIMER_START=`date +%s%N`
 
-    TIMER_START=`date +%s%N`
-
-    #A
-    # project_list=$(__project_list "$build_cfg" "$configs" "$platform")
-
-    #B
-    collect=$RECIPES/ware/build/collect_projects.php
+    collect=$RECIPES/ware/php/collect_projects.php
     TTL 1 php "$collect" "$build_cfg" "$configs" "$platform" "$build_list"
     readarray -t projects < "$build_list"
 
-    TIMER_END=$(date +%s%N)
-    TIMER_SPAN=$(($TIMER_END-$TIMER_START))
-    echo - | awk "{print $TIMER_SPAN/1000000000}"
+        TIMER_END=$(date +%s%N)
+        TIMER_SPAN=$(($TIMER_END-$TIMER_START))
+        echo - | awk "{print $TIMER_SPAN/1000000000}"
 
     IFS=$'\n' projects=("${projects[@]}")
 
     for project in ${projects[@]}; do
-        # echo "$project"
-        echo -n ""
+        echo "$project"
     done
 
     # project_count=$(echo "${projects[@]}" | wc -l)
